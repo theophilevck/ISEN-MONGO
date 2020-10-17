8ee24fe947fcde2b159f2f1480cb5c0502505d57
theovonck <theophilevonck@gmail.com>
2020-10-17 17:45:48 +0200

    sample-3 done

--- sample-3.py
+++ sample-3.py
@@ -1,7 +1,7 @@
 import requests
 import json
 from pprint import pprint
-from pymongo import MongoClient
+from pymongo import MongoClient,GEOSPHERE
 import time
 import dateutil.parser
 
@@ -10,7 +10,8 @@ db=client.get_database('mongo_Test')
 bdd=db.test
 
 def getStationNear(longitude,latitude, distance):
-    stations=bdd.find(
+    ##db.mongo_record.create_index([("geometry", "2dsphere")])
+    stations=db.mongo_record.find(
         {
         "geometry": {
             "$near": {
@@ -18,15 +19,42 @@ def getStationNear(longitude,latitude, distance):
                     "type": "Point" ,
                     "coordinates": [ longitude , latitude ]
                 },
-                "$maxDistance": distance,
-                "$minDistance": 0
+                "$maxDistance": distance
                 }
             }
         }
     )
-
+    
     nearstation = []
-
     for station in stations:
-        nearstation.append(station)
+        datas = [
+                    {
+                    "name":station.get('name'),
+                    "coordinates":station.get('geometry',{}).get('coordinates'),
+                    "bike":get_bikebyid(station.get('_id')),
+                    "stand":get_standbyid(station.get('_id'))
+                    }
+                ]
+        nearstation.append(datas)
     return nearstation
+
+def get_bikebyid(id):
+    
+    tps = db.test.find_one({ "station_id": id }, { 'bike_availbale': 1 })
+    return tps['bike_availbale']
+    
+
+def get_standbyid(id):
+    try:
+        tps = db.test.find_one({ "station_id": id }, { 'stand_availbale': 1 })
+        return tps['stand_availbale']
+    except :
+        return -1
+
+stations=getStationNear(3.130853, 50.63608, 1000)
+for station in stations:
+    pprint(station)
+
+
+
+        
